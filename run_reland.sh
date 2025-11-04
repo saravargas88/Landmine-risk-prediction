fa2b93c0b0ef82e28ce0085bab4ba684aa4565d9
saravargas88 <128425335+saravargas88@users.noreply.github.com>
2025-11-04 16:38:29 -0500

    Update run_reland.sh

--- run_reland.sh
+++ run_reland.sh
@@ -1,3 +1,11 @@
-CUDA_VISIBLE_DEVICES=0,1 python main.py --timestamp 05012023181236 \
---municipio puerto --subset full --model TabCmpt --n_step 2 \
---objective irm 2>&1 | tee -a ./experiments/05012023181236/log.txt
+timestamp=${1:-$(date +"%m_%d_%Y_%H_%M_%S")}
+mkdir -p ./experiments/$timestamp
+
+# Set PYTHONPATH to include the local pytorch_tabnet_irm directory
+export PYTHONPATH=./pytorch_tabnet_irm:.
+
+
+CUDA_VISIBLE_DEVICES=0,1 python main.py \
+   --timestamp $timestamp  \
+   --municipio puerto --subset full --model TabCmpt --n_step 2 \
+   --objective irm 2>&1 | tee -a ./experiments/$timestamp/log.txt
